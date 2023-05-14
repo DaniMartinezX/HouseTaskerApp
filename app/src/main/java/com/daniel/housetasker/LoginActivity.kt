057777f9c0fb5d7f1fedbc96bfedfe86bf6ec544
DaniMartinezX <danielmartinezcarreira@gmail.com>
2023-05-14 17:26:06 +0200

    First fragment created working well

--- app/src/main/java/com/daniel/housetasker/LoginActivity.kt
+++ app/src/main/java/com/daniel/housetasker/LoginActivity.kt
@@ -2,10 +2,38 @@ package com.daniel.housetasker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.daniel.housetasker.databinding.ActivityMainBinding
+import com.daniel.housetasker.ui.view.fragments.HomeFragment
+import com.daniel.housetasker.ui.view.fragments.HomeFragment.Companion.ARG_PARAM1
+import com.daniel.housetasker.ui.view.fragments.HomeFragment.Companion.ARG_PARAM2
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+        binding.btnHome.setOnClickListener {
+            val bundle = Bundle().apply {
+                putString(ARG_PARAM1, "Daniel")
+                putString(ARG_PARAM2, "daniel@gmail.com")
+            }
+
+            val homeFragment = HomeFragment()
+            homeFragment.arguments = bundle
+
+            binding.fragmentContainer.visibility = View.VISIBLE //Visible el HomeFragment
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, homeFragment)
+                .addToBackStack(null) // Opcional: agrega la transacción a la pila de retroceso
+                .commit()
+        }
     }
 }
